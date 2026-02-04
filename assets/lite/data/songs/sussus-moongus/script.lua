5b7b038ee6638134d469bce48245eff3d5773615
kenton54 <kentogamerisaneko@gmail.com>
2026-02-04 16:39:59 -0400

    restored Sussus Moongus intro

---
+++ assets/lite/data/songs/sussus-moongus/script.lua
@@ -0,0 +1,55 @@
+local introPlayed = false
+
+function onCreatePost()
+    setProperty('defaultCamZoom', 0.65)
+    setProperty('camGame.zoom', 0.65)
+    setProperty('camHUD.alpha', 0);
+
+    makeLuaSprite('whiteThing', '', 0, 0)
+    makeGraphic('whiteThing', 2000, 2000, 'FFFFFF')
+    setObjectCamera('whiteThing', 'camOther')
+    setProperty('whiteThing.alpha', 1)
+    addLuaSprite('whiteThing', false)
+
+    setProperty('isCameraOnForcedPos', true)
+end
+
+function onSongStart()
+    doTweenAlpha('tweenAlpha', 'whiteThing', 0, 11)
+    doTweenZoom('tweenZoom', 'camGame', 0.5, 10.5, 'quadInOut')
+    setProperty('camGame.scroll.x', -500)
+    setProperty('camGame.scroll.y', -2000)
+    setProperty('camFollow.x', -500)
+    setProperty('camFollow.y', -2000)
+end
+
+local tweened = false
+local hudvisible = false
+
+function onStepHit()
+    if not introPlayed then
+        if curStep >= 2 and not tweened then
+            tweened = true
+            doTweenY('tweenY', 'camGame.scroll', 0, 11, 'quadInOut')
+            setProperty('camFollow.y', 0)
+        end
+        if curStep >= 112 and not hudvisible then
+            hudvisible = true
+            doTweenAlpha('tweenAlpha', 'camHUD', 1, 1)
+        end
+        if curStep >= 127 then
+            setProperty('isCameraOnForcedPos', false)
+        end
+        if curStep >= 128 then
+            introPlayed = true
+            cancelTween('tweenY')
+            setProperty('defaultCamZoom', 0.6)
+        end
+    end
+end
+
+function onTweenCompleted(t)
+    if t == 'tweenZoom' then
+        setProperty('defaultCamZoom', getProperty('camGame.zoom'))
+    end
+end
--- assets/lite/stages/polus.lua
+++ assets/lite/stages/polus.lua
@@ -1,22 +1,24 @@
 function onCreatePost()
-    	if isSongSM then
-    		runHaxeCode([[
-			import flixel.addons.display.FlxBackdrop;
-			var spaceInf:FlxBackdrop = new FlxBackdrop(Paths.image("bg/polus/stars"));
-			spaceInf.setPosition(-1460, -1200);
-			spaceInf.scale.set(2.2, 2.2);
-			// spaceInf.scrollFactor.set(0.8, 0.8);
-			addBehindGF(spaceInf);
-    		]]);
-    	end
+	isSongSM = (songName == 'sussus-moongus')
 
-    	makeLuaSprite("pbg", "bg/polus/background", -1460, -1200)
-    	scaleObject("pbg", 2.2, 2.2)
-    	setProperty("pbg.antialiasing", false)
-    	addLuaSprite("pbg")
+	if isSongSM then
+		runHaxeCode([[
+		import flixel.addons.display.FlxBackdrop;
+		var spaceInf:FlxBackdrop = new FlxBackdrop(Paths.image("bg/polus/stars"));
+		spaceInf.setPosition(-1460, -1200);
+		spaceInf.scale.set(2.2, 2.2);
+		// spaceInf.scrollFactor.set(0.8, 0.8);
+		addBehindGF(spaceInf);
+		]]);
+	end
 
-    	makeLuaSprite("moon", "bg/polus/moon", -1750, -2500)
-    	scaleObject("moon", 2.2, 2.2)
-    	setProperty("moon.antialiasing", false)
-    	addLuaSprite("moon")
+	makeLuaSprite("pbg", "bg/polus/background", -1460, -1200)
+	scaleObject("pbg", 2.2, 2.2)
+	setProperty("pbg.antialiasing", false)
+	addLuaSprite("pbg")
+
+	makeLuaSprite("moon", "bg/polus/moon", -1750, -2500)
+	scaleObject("moon", 2.2, 2.2)
+	setProperty("moon.antialiasing", false)
+	addLuaSprite("moon")
 end
