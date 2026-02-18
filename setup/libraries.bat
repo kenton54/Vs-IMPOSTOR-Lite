eb569f76d7a4393d92d64e12d542435099d3dad4
MrpoloOfficial <154438885+MrpoloOfficial@users.noreply.github.com>
2026-02-19 02:45:27 +0330

    lib

---
+++ setup/libraries.bat
@@ -0,0 +1,7 @@
+haxelib set flixel 6.1.2
+haxelib set flixel-addons 4.0.1
+haxelib set flixel-ui 2.6.4
+haxelib set flixel-tools 1.5.1
+haxelib set openfl 9.3.3
+haxelib set lime 8.1.2
+haxelib set hxdiscord_rpc 1.3.0
