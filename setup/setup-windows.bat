0af244170df522760c703946c664d6496b7b0be4
kenton54 <kentogamerisaneko@gmail.com>
2026-02-18 20:42:36 -0400

    recoded workflows (again)

---
+++ setup/setup-windows.bat
@@ -0,0 +1,13 @@
+echo Installing dependencies...
+haxelib install lime
+haxelib install openfl
+haxelib install flixel
+haxelib install flixel-addons
+haxelib install flixel-tools
+haxelib install hscript-iris
+haxelib install tjson
+haxelib install hxdiscord_rpc --skip-dependencies
+haxelib install hxvlc --skip-dependencies
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit master
+echo Finished!
+pause
---
+++ setup/setup.sh
@@ -0,0 +1,13 @@
+# Setup for MacOS and Linux, Windows gets it's own setup file cuz of Microsoft's bs lol.
+echo Installing dependencies...
+haxelib install lime
+haxelib install openfl
+haxelib install flixel
+haxelib install flixel-addons
+haxelib install flixel-tools
+haxelib install hscript-iris
+haxelib install tjson
+haxelib install hxdiscord_rpc --skip-dependencies
+haxelib install hxvlc --skip-dependencies
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit master
+echo Finished!
