2618c3ed470b08fbf5441dd2b02ce5ee64404a65
kenton54 <kentogamerisaneko@gmail.com>
2026-03-24 23:28:50 -0400

    big oopsie

--- assets/lite/data/songs/defeat/script.hx
+++ assets/lite/data/songs/defeat/script.hx
@@ -1,93 +1,22 @@
+import backend.CoolUtil;
+import objects.StrumNote;
 import objects.VideoSprite;
+
+import flixel.util.FlxSort;
+
+import lime.app.Application;
+
+import Main;
 
 final MAX_MISSES:Int = 5;
 
-var introVideo:VideoSprite;
-
-function onCreate()
-{
-	introVideo = new VideoSprite(Paths.video("defeatIntro"), true);
-	introVideo.cameras = [game.camHUD];
-	introVideo.finishCallback = introFinished;
-	add(introVideo);
-}
-
-function onCreatePost()
-{
-	game.timeBar.leftBar.color = 0xFFF03636;
-
-	FlxG.camera.snapToTarget();
-}
-
-var introPlayed:Bool = false;
-
-function introFinished()
-{
-	introPlayed = true;
-}
-
-function onKeyPressPre(key)
-{
-	if (!introPlayed)
-		return Function_Stop;
-}
-
-function onUpdate(elapsed:Float)
-{
-	//updateLegacyFPSCounter();
-
-	if (Conductor.songPosition >= -(Conductor.crochet * 1.65) && !introPlayed)
-	{
-		introVideo.play();
-	}
-
-	if (game.songMisses > MAX_MISSES && !isDead)
-		game.health = 0;
-}
-
-/*
-import backend.CoolUtil;
-import flixel.util.FlxStringUtil;
-import flixel.FlxSprite;
-import lime.app.Application;
-import lime.graphics.Image;
-import objects.StrumNote;
-import objects.VideoSprite;
-import openfl.display.Bitmap;
-import openfl.display.BitmapData;
-import openfl.display.Sprite;
-import openfl.text.TextField;
-import openfl.text.TextFormat;
-import Main;
-
-var legacyFPSCounter:Sprite;
-var legacyFPSBG:Bitmap;
-var legacyFPSText:TextField;
+var isLegacy:Bool = false;
 
 var introVideo:VideoSprite;
 var fadeSprite:FlxSprite;
 
-var isLegacy:Bool = false;
-
-var oldMiddleScroll:Bool = ClientPrefs.data.middleScroll;
-
 function onCreate()
 {
-	var window = Application.current.window;
-	window.maximized = false;
-	window.resizable = false;
-	FlxG.resizeWindow(1200, 900);
-
-	window.x = (window.display.bounds.width - FlxG.stage.stageWidth) / 2;
-	window.y = (window.display.bounds.height - FlxG.stage.stageHeight) / 2;
-
-	FlxG.width = FlxG.initialWidth = 1200;
-	FlxG.height = FlxG.initialHeight = 900;
-	FlxG.resizeGame(1200, 900);
-	game.camGame.setSize(1200, 900);
-	game.camHUD.setSize(1200, 900);
-	game.camOther.setSize(1200, 900);
-
 	introVideo = new VideoSprite(Paths.video("defeatIntro"), true);
 	introVideo.cameras = [game.camHUD];
 	introVideo.finishCallback = introFinished;
@@ -99,73 +28,92 @@ function onCreate()
 	fadeSprite.visible = false;
 	add(fadeSprite);
 
-	createLegacyFPSCounter();
-
-	ClientPrefs.data.middleScroll = true;
+	if (platformTarget == 'desktop')
+	{
+		prepareDesktop();
+	}
 }
 
-function createLegacyFPSCounter()
+function prepareDesktop()
 {
-	legacyFPSCounter = new Sprite();
-	legacyFPSCounter.x = 12;
-	legacyFPSCounter.y = 3;
-	FlxG.game.addChild(legacyFPSCounter);
+	var window = Application.current.window;
 
-	legacyFPSBG = new Bitmap();
-	legacyFPSBG.bitmapData = new BitmapData(1, 1, true, 0x6F000000);
-	legacyFPSBG.x -= 2;
-	legacyFPSCounter.addChild(legacyFPSBG);
+	window.borderless = true;
+	window.maximized = false;
+	window.resizable = false;
+	FlxG.resizeWindow(1200, 900);
 
-	legacyFPSText = new TextField();
-	legacyFPSText.selectable = false;
-	legacyFPSText.mouseEnabled = false;
-	legacyFPSText.defaultTextFormat = new TextFormat("_sans", 16, FlxColor.WHITE);
-	legacyFPSText.autoSize = 1;
-	legacyFPSCounter.addChild(legacyFPSText);
-
-	legacyFPSCounter.visible = false;
+	window.x = (window.display.bounds.width - FlxG.stage.stageWidth) / 2;
+	window.y = (window.display.bounds.height - FlxG.stage.stageHeight) / 2;
 }
 
-function updateLegacyFPSCounter()
-{
-	legacyFPSText.text = "FPS: " + Main.getFPS() + " • Memory: " + FlxStringUtil.formatBytes(Main.getMemory());
-
-	legacyFPSBG.width = legacyFPSText.width + 8;
-	legacyFPSBG.height = legacyFPSText.height;
-}
-
-function removeLegacyFPSCounter()
-{
-	FlxG.game.removeChild(legacyFPSCounter);
-
-	legacyFPSText = null;
-	legacyFPSBG = null;
-	legacyFPSCounter = null;
-}
+var events:Array<Dynamic> = [
+	{
+		section: 14,
+		func: function()
+		{
+			introVideo.destroy();
+			introFinished();
+		}
+	},
+	{
+		section: 15,
+		func: function()
+		{
+			fadeInGameplay();
+		}
+	},
+	{
+		section: 33,
+		func: function()
+		{
+			fadeInBackground();
+		}
+	},
+	{
+		section: 95,
+		func: function()
+		{
+			fadeToLegacy();
+		}
+	},
+	{
+		section: 97,
+		func: function()
+		{
+			fadeIntoLegacy();
+		}
+	},
+	{
+		section: 113,
+		func: function()
+		{
+			fadeToLite();
+		}
+	},
+	{
+		section: 114,
+		func: function()
+		{
+			fadeIntoLite();
+		}
+	}
+];
 
 function onCreatePost()
 {
-	game.strumLineNotes.visible = false;
-
-	for (note in game.unspawnNotes)
-	{
-		if (!note.mustPress)
-			note.visible = false;
-	}
-
-	game.uiGroup.alpha = 0;
-
 	game.timeBar.leftBar.color = 0xFFF03636;
 
 	game.healthBar.visible = false;
 	game.updateIconsPosition = defeatIconsPosition;
 	game.updateIconsAnimation = defeatIconsAnimation;
 
-	repositionNotes();
-}
+	for (sprite in [game.iconP1, game.iconP2, game.scoreTxt, game.timeBar, game.timeTxt])
+		sprite.visible = false;
 
-function onSongStart()
-{
+	game.strumLineNotes.visible = false;
+
+	events.sort((a, b) -> FlxSort.byValues(1, a.section, b.section));
 }
 
 var introPlayed:Bool = false;
@@ -188,14 +136,14 @@ function onKeyPressPre(key)
 
 function onUpdate(elapsed:Float)
 {
-	updateLegacyFPSCounter();
+	//updateLegacyFPSCounter();
 
 	if (Conductor.songPosition >= -(Conductor.crochet * 1.65) && !introPlayed)
 	{
 		introVideo.play();
 	}
 
-	if (game.songMisses > 3 && !isDead)
+	if (game.songMisses > MAX_MISSES && !isDead)
 		game.health = 0;
 }
 
@@ -209,7 +157,12 @@ function preUpdateScore(miss:Bool)
 	}
 
 	var scorStuff:String = "Score: " + game.songScore;
-	scorStuff += !game.instakillOnMiss ? " | Misses: " + game.songMisses + " / 3" : "";
+
+	if (!game.instakillOnMiss)
+	{
+		scorStuff += " | Misses: " + game.songMisses + " / " + MAX_MISSES;
+	}
+
 	scorStuff += " | Rating: " + str;
 	scoreTxt.text = scorStuff + "\n";
 
@@ -229,14 +182,14 @@ function defeatIconsPosition()
 function defeatIconsAnimation()
 {
 	if (!iconP1.isAnimatedIcon)
-		game.iconP1.animation.curAnim.curFrame = (game.songMisses > 2) ? 1 : 0;
+		game.iconP1.animation.curAnim.curFrame = (game.songMisses >= MAX_MISSES) ? 1 : 0;
 	else
-		game.iconP1.animation.play((game.songMisses > 2) ? 'losing' : (game.songMisses < 1 ? 'winning' : 'normal'));
+		game.iconP1.animation.play((game.songMisses >= MAX_MISSES) ? 'losing' : (game.songMisses < 1 ? 'winning' : 'normal'));
 
 	if (!iconP2.isAnimatedIcon)
-		game.iconP2.animation.curAnim.curFrame = (game.songMisses <= 2) ? 1 : 0;
+		game.iconP2.animation.curAnim.curFrame = (game.songMisses < MAX_MISSES) ? 1 : 0;
 	else
-		game.iconP2.animation.play((game.songMisses < 1) ? 'losing' : (game.songMisses > 2 ? 'winning' : 'normal'));
+		game.iconP2.animation.play((game.songMisses < 1) ? 'losing' : (game.songMisses >= MAX_MISSES ? 'winning' : 'normal'));
 }
 
 function onPause()
@@ -251,78 +204,100 @@ function onResume()
 
 function onSectionHit()
 {
-	if (curSection >= 14)
+	if (events.length > 0 && curSection >= events[events.length - 1].section)
 	{
-		introVideo.destroy();
-		introPlayed = true;
-	}
+		var data:Dynamic = events.pop();
 
-    if (curSection == 15)
-    {
-        var tweenDur:Float = (Conductor.stepCrochet / 1000) * 16;
-        for (sprite in [game.healthBar, game.iconP1, game.iconP2, game.scoreTxt])
-			FlxTween.tween(sprite, {alpha: 1}, tweenDur);
-
-		game.strumLineNotes.visible = true;
-
-		var strumIndex:Int = 0;
-		for (strum in game.strumLineNotes.members)
-		{
-			strum.alpha = 0;
-			var targetAlpha:Float = 1;
-			if (strum.player < 1)
-			{
-				if (!ClientPrefs.data.opponentStrums)
-					targetAlpha = 0;
-				else if (ClientPrefs.data.middleScroll)
-					targetAlpha = 0.35;
-				targetAlpha = 0;
-			}
-			FlxTween.tween(strum, {alpha: targetAlpha}, tweenDur);
-			strumIndex++;
-		}
-    }
-    else if (curSection == 95)
-    {
-        prepareChangeToLegacy();
-    }
-	else if (curSection == 97)
-    {
-        changeToLegacy();
-    }
-	else if (curSection == 113)
-	{
-		prepareChangeToLite();
-	}
-	else if (curSection == 114)
-	{
-		changeToLite();
+		if (data.func != null)
+			data.func();
 	}
 }
 
-function prepareChangeToLegacy()
+function fadeInGameplay()
 {
-    game.canPause = false;
+	var tweenDur:Float = (Conductor.stepCrochet / 1000) * 16;
+
+	for (sprite in [game.iconP1, game.iconP2])
+	{
+		sprite.visible = !ClientPrefs.data.hideHud;
+		sprite.alpha = 0;
+		FlxTween.tween(sprite, {alpha: ClientPrefs.data.healthBarAlpha}, tweenDur);
+	}
+
+	game.scoreTxt.visible = !ClientPrefs.data.hideHud;
+
+	for (sprite in [game.timeBar, game.timeTxt])
+		sprite.visible = true;
+
+	for (sprite in [game.scoreTxt, game.timeBar, game.timeTxt])
+	{
+		sprite.alpha = 0;
+		FlxTween.tween(sprite, {alpha: 1}, tweenDur);
+	}
+
+	game.strumLineNotes.visible = true;
+
+	for (strum in game.strumLineNotes.members)
+	{
+		strum.alpha = 0;
+		var targetAlpha:Float = 1;
+
+		if (strum.player < 1)
+		{
+			if (!ClientPrefs.data.opponentStrums)
+				targetAlpha = 0;
+			else if (ClientPrefs.data.middleScroll)
+				targetAlpha = 0.35;
+		}
+
+		FlxTween.tween(strum, {alpha: targetAlpha}, tweenDur);
+	}
+}
+
+function fadeInBackground()
+{
+	var bgCorpses:FlxSprite = game.getLuaObject("bgCorpses");
+	var leftCorpses:FlxSprite = game.getLuaObject("leftCorpses");
+	var rightCorpses:FlxSprite = game.getLuaObject("rightCorpses");
+	var light:FlxSprite = game.getLuaObject("light");
+
+	for (spr in [bgCorpses, leftCorpses, rightCorpses, light])
+	{
+		FlxTween.tween(spr, {alpha: 1}, 1);
+	}
+}
+
+function fadeToLegacy()
+{
+	game.canPause = false;
 
 	fadeSprite.visible = true;
 
 	var sectionDur:Float = (Conductor.stepCrochet / 1000) * 16;
 	FlxTween.tween(fadeSprite, {alpha: 1}, sectionDur);
 
-	Main.fpsCounter.alpha = 1;
-	FlxTween.tween(Main.fpsCounter, {alpha: 0}, sectionDur);
+	//Main.fpsCounter.alpha = 1;
+	//FlxTween.tween(Main.fpsCounter, {alpha: 0}, sectionDur);
 
-	var window = Application.current.window;
-	var windowMidPoint:FlxPoint = FlxPoint.get(window.x + window.width / 2, window.y + window.height / 2);
-	FlxTween.tween(window, {width: 1280, height: 720}, sectionDur, {
-        startDelay: sectionDur,
-        ease: FlxEase.quartInOut,
-        onUpdate: _ -> {
-			window.x = windowMidPoint.x - window.width / 2;
-			window.y = windowMidPoint.y - window.height / 2;
-        },
-        onComplete: _ -> setUpLegacy()
-    });
+	if (platformTarget == "desktop")
+	{
+		var window = Application.current.window;
+		var windowMidPoint:FlxPoint = FlxPoint.get(window.x + window.width / 2, window.y + window.height / 2);
+		FlxTween.tween(window, {width: 1280, height: 720}, sectionDur, {
+			startDelay: sectionDur,
+			ease: FlxEase.quartInOut,
+			onUpdate: _ ->
+			{
+				window.x = windowMidPoint.x - window.width / 2;
+				window.y = windowMidPoint.y - window.height / 2;
+			},
+			onComplete: _ -> setUpLegacy()
+		});
+	}
+	else
+	{
+		new FlxTimer().start(sectionDur, _ -> setUpLegacy());
+	}
 }
 
 function setUpLegacy()
@@ -334,12 +309,15 @@ function setUpLegacy()
 	game.changeUIStyle("legacy");
 	game.legacyCameraMove = true;
 
-	FlxG.width = FlxG.initialWidth = 1280;
-	FlxG.height = FlxG.initialHeight = 720;
-	FlxG.resizeGame(1280, 720);
-    game.camGame.setSize(1280, 720);
-	game.camHUD.setSize(1280, 720);
-	game.camOther.setSize(1280, 720);
+	if (platformTarget == "desktop")
+	{
+		FlxG.width = FlxG.initialWidth = 1280;
+		FlxG.height = FlxG.initialHeight = 720;
+		FlxG.resizeGame(1280, 720);
+		game.camGame.setSize(1280, 720);
+		game.camHUD.setSize(1280, 720);
+		game.camOther.setSize(1280, 720);
+	}
 
 	repositionNotes();
 
@@ -377,7 +355,40 @@ function setUpLegacy()
 	game.timeBar.leftBar.color = 0xFFFF0000;
 }
 
-function changeToLegacy()
+function repositionNotes()
+{
+	var strumX:Float = ClientPrefs.data.middleScroll ? (isLegacy ? -278 : PlayState.STRUM_X_MIDDLESCROLL) : PlayState.STRUM_X;
+	var strumY:Float = ClientPrefs.data.downScroll ? (FlxG.height - 150) : 50;
+
+	var strumIndex:Int = 0;
+	for (strum in strumLineNotes.members)
+	{
+		strum.x = strumX;
+		strum.y = strumY;
+
+		if (strumIndex <= 3) // opponent
+		{
+			if (ClientPrefs.data.middleScroll)
+			{
+				strum.x += 310;
+				if (strumIndex > 1)
+					strum.x += FlxG.width / 2 + 25;
+			}
+		}
+
+		postPositionStrum(strum);
+		strumIndex++;
+	}
+}
+
+function postPositionStrum(strum:StrumNote)
+{
+	strum.x += Note.swagWidth * strum.noteData;
+	strum.x += isLegacy ? 50 : 17.5;
+	strum.x += (FlxG.width / 2) * strum.player;
+}
+
+function fadeIntoLegacy()
 {
 	game.canPause = true;
 	game.doIconScale = true;
@@ -385,17 +396,17 @@ function changeToLegacy()
 
 	game.scoreTxt.color = FlxColor.RED;
 
-	Application.current.window.title = "Impostor Legacy";
-	Main.fpsCounter.visible = false;
-	legacyFPSCounter.visible = true;
+	//Application.current.window.title = "Impostor Legacy";
+	//Main.fpsCounter.visible = false;
+	//legacyFPSCounter.visible = true;
 
 	FlxTween.tween(fadeSprite, {alpha: 0}, 1);
 
-	legacyFPSCounter.alpha = 0;
-	FlxTween.tween(legacyFPSCounter, {alpha: 1}, 1);
+	//legacyFPSCounter.alpha = 0;
+	//FlxTween.tween(legacyFPSCounter, {alpha: 1}, 1);
 }
 
-function prepareChangeToLite()
+function fadeToLite()
 {
 	game.canPause = false;
 
@@ -403,46 +414,54 @@ function prepareChangeToLite()
 	var halfSection:Float = (Conductor.stepCrochet / 1000) * 8;
 	FlxTween.tween(fadeSprite, {alpha: 1}, fadeDur);
 
-	legacyFPSCounter.alpha = 1;
-	FlxTween.tween(legacyFPSCounter, {alpha: 0}, fadeDur);
+	//legacyFPSCounter.alpha = 1;
+	//FlxTween.tween(legacyFPSCounter, {alpha: 0}, fadeDur);
 
-	var window = Application.current.window;
-	var windowMidPoint:FlxPoint = FlxPoint.get(window.x + window.width / 2, window.y + window.height / 2);
-	FlxTween.tween(window, {width: 1200, height: 900}, halfSection, {
-		startDelay: halfSection,
-		ease: FlxEase.quartInOut,
-		onUpdate: _ -> {
-			FlxG.width = FlxG.initialWidth = window.width;
-			FlxG.height = FlxG.initialHeight = window.height;
-			FlxG.resizeGame(window.width, window.height);
-			game.camGame.setSize(window.width, window.height);
-			game.camHUD.setSize(window.width, window.height);
-			game.camOther.setSize(window.width, window.height);
-
-			repositionNotes();
-
-			fadeSprite.setGraphicSize(FlxG.width, FlxG.height);
-			fadeSprite.updateHitbox();
-			fadeSprite.screenCenter();
-
-			game.healthBar.screenCenter(0x01);
-			game.healthBar.y = FlxG.height * (!ClientPrefs.data.downScroll ? 0.89 : 0.11);
-			game.iconP1.y = game.iconP2.y = game.healthBar.y - 70;
-			game.scoreTxt.y = game.healthBar.y + 40;
-
-			scoreTxt.fieldWidth = FlxG.width;
-
-			game.botplayTxt.y = ClientPrefs.data.downScroll ? game.timeBar.y - 78 : game.timeBar.y + 55;
-			game.botplayTxt.fieldWidth = FlxG.width;
-
-			game.timeTxt.fieldWidth = FlxG.width;
-			game.timeBar.screenCenter(0x01);
-
-			window.x = windowMidPoint.x - window.width / 2;
-			window.y = windowMidPoint.y - window.height / 2;
-		},
-		onComplete: _ -> setUpLite()
-	});
+	if (platformTarget == "desktop")
+	{
+		var window = Application.current.window;
+		var windowMidPoint:FlxPoint = FlxPoint.get(window.x + window.width / 2, window.y + window.height / 2);
+		FlxTween.tween(window, {width: 1200, height: 900}, halfSection, {
+			startDelay: halfSection,
+			ease: FlxEase.quartInOut,
+			onUpdate: _ ->
+			{
+				FlxG.width = FlxG.initialWidth = window.width;
+				FlxG.height = FlxG.initialHeight = window.height;
+				FlxG.resizeGame(window.width, window.height);
+				game.camGame.setSize(window.width, window.height);
+				game.camHUD.setSize(window.width, window.height);
+				game.camOther.setSize(window.width, window.height);
+	
+				repositionNotes();
+	
+				fadeSprite.setGraphicSize(FlxG.width, FlxG.height);
+				fadeSprite.updateHitbox();
+				fadeSprite.screenCenter();
+	
+				game.healthBar.screenCenter(0x01);
+				game.healthBar.y = FlxG.height * (!ClientPrefs.data.downScroll ? 0.89 : 0.11);
+				game.iconP1.y = game.iconP2.y = game.healthBar.y - 70;
+				game.scoreTxt.y = game.healthBar.y + 40;
+	
+				scoreTxt.fieldWidth = FlxG.width;
+	
+				game.botplayTxt.y = ClientPrefs.data.downScroll ? game.timeBar.y - 78 : game.timeBar.y + 55;
+				game.botplayTxt.fieldWidth = FlxG.width;
+	
+				game.timeTxt.fieldWidth = FlxG.width;
+				game.timeBar.screenCenter(0x01);
+	
+				window.x = windowMidPoint.x - window.width / 2;
+				window.y = windowMidPoint.y - window.height / 2;
+			},
+			onComplete: _ -> setUpLite()
+		});
+	}
+	else
+	{
+		new FlxTimer().start(halfSection, _ -> setUpLite());
+	}
 }
 
 function setUpLite()
@@ -490,57 +509,57 @@ function setUpLite()
 	game.timeBar.leftBar.color = 0xFFF03636;
 }
 
-function changeToLite()
+function exitLegacy()
+{
+	isLegacy = false;
+
+	if (platformTarget == 'desktop')
+	{
+		FlxG.width = FlxG.initialWidth = 1200;
+		FlxG.height = FlxG.initialHeight = 900;
+		FlxG.resizeGame(1200, 900);
+		game.camGame.setSize(1200, 900);
+		game.camHUD.setSize(1200, 900);
+		game.camOther.setSize(1200, 900);
+	}
+}
+
+function fadeIntoLite()
 {
 	game.canPause = true;
 	game.doIconScale = false;
 	game.dynamicScoreColors = true;
 
-	Application.current.window.title = "Vs. Impostor: Lite";
-	Main.fpsCounter.visible = true;
-	legacyFPSCounter.visible = false;
+	//Application.current.window.title = "Vs. Impostor: Lite";
+	//Main.fpsCounter.visible = true;
+	//legacyFPSCounter.visible = false;
 
 	FlxTween.tween(fadeSprite, {alpha: 0}, 1);
 
-	Main.fpsCounter.alpha = 0;
-	FlxTween.tween(Main.fpsCounter, {alpha: 1}, 1);
+	//Main.fpsCounter.alpha = 0;
+	//FlxTween.tween(Main.fpsCounter, {alpha: 1}, 1);
 }
 
-function repositionNotes()
+function onDestroy()
 {
-	var strumX:Float = ClientPrefs.data.middleScroll ? (isLegacy ? -278 : PlayState.STRUM_X_MIDDLESCROLL) : PlayState.STRUM_X;
-	var strumY:Float = ClientPrefs.data.downScroll ? (FlxG.height - 150) : 50;
+	Main.fpsCounter.visible = true;
 
-	var strumIndex:Int = 0;
-	for (strum in strumLineNotes.members)
+	if (platformTarget == 'desktop')
 	{
-		strum.x = strumX;
-		strum.y = strumY;
-
-		if (strumIndex <= 3) // opponent
-		{
-			if (ClientPrefs.data.middleScroll)
-			{
-				strum.x += 310;
-				if (strumIndex > 1) strum.x += FlxG.width / 2 + 25;
-			}
-		}
-
-		postPositionStrum(strum);
-		strumIndex++;
+		restoreDesktop();
 	}
+
+	Note.swagWidth = 160 * 0.75;
 }
 
-function postPositionStrum(strum:StrumNote)
+function restoreDesktop()
 {
-	strum.x += Note.swagWidth * strum.noteData;
-	strum.x += isLegacy ? 50 : 17.5;
-	strum.x += (FlxG.width / 2) * strum.player;
-}
+	var window = Application.current.window;
 
-function exitLegacy()
-{
-	isLegacy = false;
+	window.borderless = false;
+	window.maximized = false;
+	window.resizable = true;
+	FlxG.resizeWindow(1200, 900);
 
 	FlxG.width = FlxG.initialWidth = 1200;
 	FlxG.height = FlxG.initialHeight = 900;
@@ -548,36 +567,9 @@ function exitLegacy()
 	game.camGame.setSize(1200, 900);
 	game.camHUD.setSize(1200, 900);
 	game.camOther.setSize(1200, 900);
+	game.camCountdown.setSize(1200, 900);
+	game.camDialogue.setSize(1200, 900);
+
+	window.x = (window.display.bounds.width - FlxG.stage.stageWidth) / 2;
+	window.y = (window.display.bounds.height - FlxG.stage.stageHeight) / 2;
 }
-
-function onEndSong()
-{
-	if (isLegacy)
-    {
-		exitLegacy();
-    }
-
-	Application.current.window.resizable = true;
-	FlxG.resizeWindow(1200, 900);
-}
-
-function onDestroy()
-{
-	removeLegacyFPSCounter();
-	Main.fpsCounter.visible = true;
-
-	Application.current.window.title = "Vs. Impostor: Lite";
-
-	if (isLegacy)
-	{
-		exitLegacy();
-	}
-
-	Application.current.window.resizable = true;
-	FlxG.resizeWindow(1200, 900);
-
-	Note.swagWidth = 160 * 0.75;
-
-	ClientPrefs.data.middleScroll = oldMiddleScroll;
-}
-*/
--- assets/lite/stages/defeat.lua
+++ assets/lite/stages/defeat.lua
@@ -8,27 +8,28 @@ function onCreate()
     scaleObject("mainBG", 2.2, 2.2)
     addLuaSprite("mainBG")
 
-    --[[
-    makeLuaSprite("bgCorpses", "bg/defeat/bg-corpses", -1700, -1200)
-    scaleObject("bgCorpses", 4.2, 4)
-    setProperty("bgCorpses.angle", -4)
+    makeLuaSprite("bgCorpses", "bg/defeat/bg-corpses", -1520, -600)
+    scaleObject("bgCorpses", 2, 2)
+    setProperty("bgCorpses.alpha", 0)
     addLuaSprite("bgCorpses")
-    ]]
 
     makeLuaSprite("light", "bg/defeat/light", -1480, -1200)
     scaleObject("light", 2, 2)
     setScrollFactor("light", 0.9, 0.9)
     setBlendMode("light", "add")
+    setProperty("light.alpha", 0)
     addLuaSprite("light", true)
 
     makeLuaSprite("leftCorpses", "bg/defeat/left-corpses", -2200, -440)
     scaleObject("leftCorpses", 2.8, 2.8)
     setScrollFactor("leftCorpses", 1.6, 1.6)
+    setProperty("leftCorpses.alpha", 0)
     addLuaSprite("leftCorpses", true)
 
     makeLuaSprite("rightCorpses", "bg/defeat/right-corpses", -890, -480)
     scaleObject("rightCorpses", 2.8, 2.8)
     setScrollFactor("rightCorpses", 1.6, 1.6)
+    setProperty("rightCorpses.alpha", 0)
     addLuaSprite("rightCorpses", true)
 
     makeLuaSprite("vignette", "bg/defeat/vignette", 0, 0)
