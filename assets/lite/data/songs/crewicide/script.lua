16424e5ea2ddf96a2557d3359c9d6875a0947333
kenton54 <kentogamerisaneko@gmail.com>
2026-03-27 10:26:41 -0400

    crewicide remaster

--- assets/lite/data/songs/crewicide/script.lua
+++ assets/lite/data/songs/crewicide/script.lua
@@ -1,32 +1,54 @@
 function onCreate()
-	makeLuaSprite("backgroundOfficeBroken", "bg/office/officeBroken", -1250, -1050);
-	scaleObject("backgroundOfficeBroken", 1.8, 1.8);
-	setProperty("backgroundOfficeBroken.antialiasing", false);
-	setProperty("backgroundOfficeBroken.visible", false);
-	addLuaSprite("backgroundOfficeBroken");
+	makeLuaSprite("backgroundOfficeBroken", "bg/office/officeBroken", -1250, -1050)
+	scaleObject("backgroundOfficeBroken", 1.8, 1.8)
+	setProperty("backgroundOfficeBroken.antialiasing", false)
+	setProperty("backgroundOfficeBroken.visible", false)
+	addLuaSprite("backgroundOfficeBroken")
 end
+
+local events = {
+	{
+		step = 2032,
+		func = function()
+			setProperty('defaultCamZoom', 0.82)
+			setProperty('opponentCameraOffset[0]', -75)
+			setProperty('opponentCameraOffset[1]', -40)
+		end,
+		executed = false
+	},
+	{
+		step = 2066,
+		func = function()
+			playAnim("dad", "jump")
+			setProperty("dad.stunned", true)
+		end,
+		executed = false
+	},
+	{
+		step = 2080,
+		func = function()
+			cameraShake('camGame', 0.02, 1.6)
+			cameraShake('camHUD', 0.01, 1.6)
+			setProperty('dad.visible', false)
+			setProperty('backgroundOffice.visible', false)
+			setProperty('backgroundOfficeBroken.visible', true)
+			setProperty('defaultCamZoom', 0.75)
+			setProperty('cameraSpeed', 6)
+			setProperty('opponentCameraOffset[0]', -200)
+			setProperty('opponentCameraOffset[1]', -200)
+		end,
+		executed = false
+	}
+}
 
 function onStepHit()
-	if curStep == 2032 then
-		setProperty('defaultCamZoom', 0.82);
-		setProperty('opponentCameraOffset[0]', -75);
-		setProperty('opponentCameraOffset[1]', -40);
-	elseif curStep == 2080 then
-		cameraShake('camGame', 0.02, 1.6);
-		cameraShake('camHUD', 0.01, 1.6);
-		setProperty('dad.visible', false);
-		setProperty('backgroundOffice.visible', false);
-		setProperty('backgroundOfficeBroken.visible', true);
-		setProperty('defaultCamZoom', 0.75);
-		setProperty('cameraSpeed', 6);
-		setProperty('opponentCameraOffset[0]', -200);
-		setProperty('opponentCameraOffset[1]', -200);
-	end
-end
+	for _, eventData in ipairs(events) do
+		if curStep >= eventData.step and not eventData.executed then
+			if eventData.func then
+				eventData.func()
+			end
 
-function onUpdate(e)
-	if getProperty('FlxG.camera.zoom') == 0.75 then --This doesnt work rn, and it sucks, ill fix it later
-		setProperty('camZooming', false);
-		setProperty('cameraSpeed', 1);
+			eventData.executed = true
+		end
 	end
 end
