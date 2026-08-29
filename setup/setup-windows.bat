101330468650d53d6c7343168e69af5a109a6f93
kenton54 <kentogamerisaneko@gmail.com>
2026-08-29 18:37:20 -0300

    p

--- setup/setup-windows.bat
+++ setup/setup-windows.bat
@@ -1,18 +1,18 @@
 echo Installing dependencies...
 haxelib install lime --quiet
 haxelib install openfl --quiet
-haxelib install flixel --quiet
-haxelib install flixel-addons --quiet
+haxelib install flixel --skip-dependencies --quiet
+haxelib install flixel-addons --skip-dependencies --quiet
 haxelib install flixel-tools --quiet
 haxelib install flixel-ui --quiet
 haxelib install hscript-iris --quiet
 haxelib install hxcpp-debug-server --quiet
 haxelib install hxdiscord_rpc --skip-dependencies --quiet
 haxelib install hxvlc --skip-dependencies --quiet
-haxelib git linc_luajit https://github.com/superpowers04/linc_luajit master --quiet
-haxelib install extension-androidtools --quiet
-haxelib install hxgamemode --quiet
-haxelib git hxcpp https://github.com/FunkinCrew/hxcpp c79483a7bf1c0afa77d35a8c564cecac83d5c890 --quiet
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit master
+haxelib install extension-androidtools --skip-dependencies --quiet
+haxelib install hxgamemode --skip-dependencies --quiet
+haxelib git hxcpp https://github.com/FunkinCrew/hxcpp c79483a7bf1c0afa77d35a8c564cecac83d5c890
 haxelib install hxp --quiet
 haxelib install format --quiet
 echo Finished!
